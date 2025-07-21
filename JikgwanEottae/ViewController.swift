32cb387af7a61adf33254d0633d1777c0ba8cd1b
hoon <stitch8971@gachon.ac.kr>
2025-07-22 00:12:54 +0900

    Initial Commit

---
+++ JikgwanEottae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JikgwanEottae
+//
+//  Created by 7aeHoon on 7/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
